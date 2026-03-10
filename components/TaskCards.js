e5f14039b34eb7d8258f716eb404fae912e843d5
DenzelMwangi <159570611+WachiraMwangiDenzel@users.noreply.github.com>
2026-03-10 21:45:42 +0300

    added colors to the header of the task cards and a shadow on the cards

--- components/TaskCards.js
+++ components/TaskCards.js
@@ -3,18 +3,18 @@ import React from 'react'
 export default function TaskCards() {
   return (
     <div className='text-black mt-5 grid grid-cols-3 gap-4 rounded-lg'>
-      <div className='bg-[#87848447] m-3 p-4 rounded-lg'>
-        <h1 className=' '>Design New Logo</h1>
+      <div className='bg-[#87848447] m-3 p-4 rounded-lg  shadow'>
+        <h1 className='text-[#076b50]'>Design New Logo</h1>
         <p className="text-sm font-normal  "><span>Deadline: </span>April 31, 2023</p>
         <p className="text-sm font-normal  "><span>Status: </span>In Progress</p>
       </div>
-        <div className='bg-[#87848447] m-3 p-4 rounded-lg'>
-        <h1>Mobile App Development</h1>
+        <div className='bg-[#87848447] m-3 p-4 rounded-lg shadow'>
+        <h1 className='text-[#076b50]'>Mobile App Development</h1>
         <p className="text-sm font-normal  "><span>Deadline: </span>August 31, 2023</p>
         <p className="text-sm font-normal  "><span>Status: </span>Not Started</p>
       </div>
-      <div className='bg-[#87848447] m-3 p-4 rounded-lg'>
-        <h1>Update Website</h1>
+      <div className='bg-[#87848447] m-3 p-4 rounded-lg shadow'>
+        <h1 className='text-[#076b50]'>Update Website</h1>
         <p className="text-sm font-normal   "><span>Deadline: </span>Feb 31, 2023</p>
         <p className="text-sm font-normal   "><span>Status: </span>Completed</p>
       </div>
