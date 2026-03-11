bbb49e934a970c9c84a5ade1564e086bc2baad26
DenzelMwangi <159570611+WachiraMwangiDenzel@users.noreply.github.com>
2026-03-11 19:56:36 +0300

    Todays Task code clean up

--- components/TodaysTask.js
+++ components/TodaysTask.js
@@ -6,7 +6,7 @@ export default function TodaysTask() {
             <div className='ml-5 text-black font-normal text-md'>
                <h1 className='font-bold '>Todays Task</h1>
                 <p className=" text-sm mb-4 -mt-1 "> Check Your Daily Tasks and Schedule</p>
-                <button className="bg-[#00ffbb] ml-7 px-4 py-2 rounded-2xl font-medium text-sm hover:bg-[#00cc99] hover:font-semibold  transition-colors duration-300 ease-in-out">
+                <button className="bg-[#008080] ml-7 px-4 py-2 rounded-2xl font-medium text-lg hover:bg-[##034545]  hover:text-orange-300  transition-colors duration-300 ease-in-out">
                     Schedule
                 </button>
             </div>
