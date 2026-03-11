d3dac3a48401bc138e7b34ec25e1e5be72ce58a4
DenzelMwangi <159570611+WachiraMwangiDenzel@users.noreply.github.com>
2026-03-11 18:58:14 +0300

    navbar customisation and removed page margins

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='bg-white px-20 py-10  flex flex-col h-screen text-2xl font-bold'>
+    <div className='bg-white flex flex-col h-screen text-2xl font-bold'>
         <Navbar />
         <TodaysTask />
         <TaskCards />
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { MessageCircle, Bell } from 'lucide-react'
+import { House } from 'lucide-react'
 
 export default function Navbar() {
   return (
-    <div className='bg-[#87848447] top-0 h-16 w-full rounded-bl-sm flex items-center justify-between px-4 text-white font-bold'>
-      <div>Dashboard</div>
-      <div className="flex-1 flex justify-center">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="px-4 py-2 rounded-md bg-white text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+    <div className='bg-[#87848447] ml-5 mr-5 mt-2 h-16  rounded-4xl flex items-center justify-between px-4 text-white font-bold transition-all duration-300 ease-in-out '>
+      <div className=' px-8 text-black font-bold text-xl'>Dashboard</div>
+      <div className="flex items-center space-x-4 ">
+       <House className='text-black font-medium text-md  cursor-pointer hover:text-[#076b50]'/> 
+       <input type="text" placeholder="Search, Tasks, Projects or Messages..." className="pl-4 pr-40 py-2 rounded-full bg-white hover:bg-[#f7f7f7] text-black font-medium text-sm placeholder-gray-400 focus:placeholder-transparent focus:outline-none focus:bg-[#f7f7f7] focus:ring-1 focus:ring-[rgb(0,0,0)]"/>
       </div>
-      <div className="flex space-x-4">
-        <MessageCircle className="w-6 h-6 cursor-pointer" />
-        <Bell className="w-6 h-6 cursor-pointer" />
+      <div className="pr-8 flex space-x-5 items-center">
+        <MessageCircle className="text-black w-6 h-6 cursor-pointer font-medium hover:text-[#076b50]" />
+        <Bell className="text-black w-6 h-6 cursor-pointer
+         font-medium hover:text-[#076b50]" />
       </div>
     </div>
   )
