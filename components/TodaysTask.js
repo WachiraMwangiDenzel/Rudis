0ac339d3be5c7749e9107724a404884f9da3ccb2
DenzelMwangi <159570611+WachiraMwangiDenzel@users.noreply.github.com>
2026-03-11 19:43:31 +0300

    Todays Task code clean up

--- components/TodaysTask.js
+++ components/TodaysTask.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function TodaysTask() {
   return (
-    <div className='bg-[#87848447] mt-10 rounded-lg items-center justify-end p-6 w-full flex flex-col md:flex-row gap-4'>
-            <div className='flex flex-col items-start gap-4 text-black text-xl font-bold'>
-                Todays Task
-                <p className="text-base font-normal -mt-4 mb-5 "> Check Your Daily Tasks and Schedule</p>
-                <button className="bg-[#00ffbb] px-4 py-2 rounded-md text-sm font-bold hover:bg-[#00cc99]">
+    <div className='bg-[#87848447] flex flex-row items-center p-4 ml-5 mt-2 mr-5 rounded-4xl drop-shadow[(0 20px 13px rgba(0, 0, 0, 0.03))]'>
+            <div className='ml-5 text-black font-normal text-md'>
+               <h1 className='font-bold '>Todays Task</h1>
+                <p className=" text-sm mb-4 -mt-1 "> Check Your Daily Tasks and Schedule</p>
+                <button className="bg-[#00ffbb] ml-7 px-4 py-2 rounded-2xl font-medium text-sm hover:bg-[#00cc99] hover:font-semibold  transition-colors duration-300 ease-in-out">
                     Schedule
                 </button>
             </div>
