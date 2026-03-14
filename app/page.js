05ebb04dd125411a1c446ed1bccb9edfc817c938
DenzelMwangi <159570611+WachiraMwangiDenzel@users.noreply.github.com>
2026-03-14 14:42:11 +0300

    added footer to the app

--- app/page.js
+++ app/page.js
@@ -3,15 +3,16 @@ import TaskCards from '@/components/TaskCards'
 import TodaysTask from '@/components/TodaysTask'
 import GraphMessages from '@/components/GraphMessages'
 import React from 'react'
+import Footer from '@/components/footer'
 
 export default function page() {
   return (
-    <div className='bg-white flex flex-col min-h-screen text-2xl font-bold mb-10 '>
+    <div className='bg-white flex flex-col min-h-screen text-2xl font-bold  '>
         <Navbar />
         <TodaysTask />
         <TaskCards />
         <GraphMessages/>
-
+        <Footer/>
     </div>
   )
 }
